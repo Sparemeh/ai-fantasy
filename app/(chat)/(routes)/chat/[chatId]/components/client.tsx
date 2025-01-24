50f632f8dbec9aabf2b41d0c4322b15925ecaf37
Sparemeh <66799519+Sparemeh@users.noreply.github.com>
2025-01-24 03:32:20 -0800

    finished Chat Message UI, changed theme colours

--- app/(chat)/(routes)/chat/[chatId]/components/client.tsx
+++ app/(chat)/(routes)/chat/[chatId]/components/client.tsx
@@ -1,7 +1,13 @@
 "use client";
 
+import { useCompletion } from "ai/react";
 import { ChatHeader } from "@/components/chat-header";
 import { Character, Message } from "@prisma/client";
+import { useRouter } from "next/navigation";
+import { FormEvent, useState } from "react";
+import { ChatForm } from "@/components/chat-form";
+import { ChatMessages } from "@/components/chat-messages";
+import { ChatMessageProps } from "@/components/chat-message";
 
 interface ChatClientProps {
     character: Character & {
@@ -13,9 +19,45 @@ interface ChatClientProps {
 };
 
 export const ChatClient = ({ character }: ChatClientProps) => {
+    const router = useRouter();
+    const [messages, setMessages] = useState<ChatMessageProps[]>(character.messages);
+
+    const {
+        input,
+        isLoading,
+        handleInputChange,
+        handleSubmit,
+        setInput,
+    } = useCompletion({
+        api: `/api/chat/${character.id}`,
+        onFinish(prompt, completion) {
+            const systemMessage: ChatMessageProps = {
+                role: "system",
+                content: completion,
+            };
+
+            setMessages((current) => [...current, systemMessage]);
+            setInput("");
+
+            router.refresh();
+        },
+    });
+
+    const onSubmit = (e: FormEvent<HTMLFormElement>) => {
+        const userMessage: ChatMessageProps = {
+            role: "user",
+            content: input,
+        };
+
+        setMessages((current) => [...current, userMessage]);
+        handleSubmit(e);
+    }
+
     return (
         <div className="flex flex-col h-full p-4 space-y-2">
             <ChatHeader character={character} />
+            <ChatMessages character={character} isLoading={isLoading} messages={messages} />
+            <ChatForm isLoading={isLoading} input={input} handleInputChange={handleInputChange} onSubmit={onSubmit} />
         </div>
     )
 }
--- app/(root)/(routes)/character/[characterId]/page.tsx
+++ app/(root)/(routes)/character/[characterId]/page.tsx
@@ -1,5 +1,6 @@
 import prismadb from "@/lib/prismadb";
 import { CharacterForm } from "./components/character-form";
+import { auth } from "@clerk/nextjs/server";
 
 interface CharacterIdPageProps {
     params: {
@@ -12,11 +13,19 @@ interface CharacterIdPageProps {
 const CharacterIdPage = async ({
     params
 }: CharacterIdPageProps) => {
+    const { userId, redirectToSignIn } = await auth();
     // TODO: Check Subscription
+
+    
+
+    if (!userId) {
+        return redirectToSignIn();
+    }
 
     const character = await prismadb.character.findUnique({
         where: {
             id: params.characterId,
+            userId
         }
     });
 
